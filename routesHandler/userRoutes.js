99f67b07edec7c90f180e565b7b7eb0f27860605
Muhammad SA <MuhammadSA211999>
2023-03-23 00:06:12 +0600

    token authnticated

--- routesHandler/userRoutes.js
+++ routesHandler/userRoutes.js
@@ -6,7 +6,7 @@ const userSchema = require('../schemas/userSchema')
 //create mongoose instance model
 const User = new mongoose.model('User', userSchema)
 
-
+//SIGNUP USER
 router.post('/signup', async (req, res) => {
     const { name, username, password, phone } = req.body
     try {
@@ -15,7 +15,36 @@ router.post('/signup', async (req, res) => {
         const newUser = new User({ name, username, phone, password: hased })
         await newUser.save()
         res.status(200).json({ message: 'created successfully' })
-    } catch (error) {
+    }
+    catch (error) {
+        // console.log(error);
+
+        res.status(500).json({ error: 'cant created' })
+    }
+})
+
+//LOGIN USER WITH SECRET-TOKEN
+router.post('/login', async (req, res) => {
+    const { password, id } = req.body
+    try {
+        const user = await User.findById({ _id: id })
+        if (user?.id) {
+            const isMatch = await bcrypt.compare(password, user?.password)
+            if (isMatch) {
+                //create a token and send it to user 
+
+            }
+            else {
+                res.status(401).json({ error: 'Authentication failed' })
+            }
+        }
+        else {
+            res.status(401).json({ error: 'Authentication failed' })
+        }
+        //bcrypt async wayete kaj kore ebong callback e result dey; ejonno try catch blocke create kora hice
+
+    }
+    catch (error) {
         // console.log(error);
 
         res.status(500).json({ error: 'cant created' })
